499f034c3e51a8446b819a218f2c861a544e34d7
jambis <imjaamees@gmail.com>
2020-03-07 23:09:25 -0600

    Added middleware for exercises route

---
+++ middleware/exercises-middleware.js
@@ -0,0 +1,34 @@
+const dbExercises = require("../models/exercises-models");
+
+module.exports = { exerciseCreator, exerciseIdExists };
+
+async function exerciseCreator(req, res, next) {
+  try {
+    const exercise = await dbExercises.getBy({ id: req.params.id });
+    if (req.decodedJwt.id != exercise.user_id) {
+      res.status(403).json({
+        message: "Sorry you aren't the creator of the exercise"
+      });
+    } else {
+      next();
+    }
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json({ error: "Failed to verify creator of the exercise" });
+  }
+}
+
+async function exerciseIdExists(req, res, next) {
+  try {
+    const exercise = await dbExercises.getBy(req.params.id);
+    exercise
+      ? next()
+      : res
+          .status(404)
+          .json({ message: "Exercise with that ID was not found" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to verify exercise ID" });
+  }
+}
